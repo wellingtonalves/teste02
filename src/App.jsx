07aae34960b9066301282ebcb87cd692803f61d2
Wellington Alves Gomes <wellington.alves.gomes@gmail.com>
2021-01-19 17:05:23 -0300

    alterado novamente

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Comp1 from "./components/Comp1";
 const App = () => {
     return (
         <div id="app">
-            <div>React 02</div>
+            <div>React 02 - TESTE</div>
             <Comp1 nome="wellington" max={222220}/>
         </div>
     )
