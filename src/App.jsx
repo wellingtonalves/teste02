5ceaa9e6416785404f46fe1a93fccb9194718487
Wellington Alves Gomes <wellington.alves.gomes@gmail.com>
2021-01-19 16:59:22 -0300

    Alteracao no nome do React

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Comp1 from "./components/Comp1";
 const App = () => {
     return (
         <div id="app">
-            <div>React</div>
+            <div>React 02</div>
             <Comp1 nome="wellington" max={222220}/>
         </div>
     )
